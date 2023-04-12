5ad12f0bd170a4001f4cb518bab34e8cec9a7631
GustavoBryan <devgustavob@gmail.com>
2023-04-12 16:38:59 -0300

    js

---
+++ services/produto-servicos.js
@@ -0,0 +1,47 @@
+const listaProdutos = () =>
+    fetch("http://localhost:3000/produto")
+        .then((resposta) => resposta.json())
+        .catch((error) => console.log(error));
+
+const listarUmProduto = (id) => {
+    return fetch(`http://localhost:3000/produto/${id}`).then((resposta) => {
+        return resposta.json();
+    });
+};
+
+const criaProdutos = (name, imageUrl, price) => {
+return fetch(`http://localhost:3000/produto`, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        name, 
+        imageUrl,
+        price,
+    }),
+}).then((resposta) => {
+    if (resposta.ok) {
+        return resposta.body;
+    }
+    throw new Error("Não foi possível criar um produto");
+});
+};
+
+const alteraProduto = async(id, name, price, description) => {
+    return fetch(`http://localhost:3000/produto/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name, 
+            price,
+            description, 
+        }),
+    })
+    .then((resposta) => {
+        return resposta.json();
+    })
+    .catch((error) => console.log(error));
+};
