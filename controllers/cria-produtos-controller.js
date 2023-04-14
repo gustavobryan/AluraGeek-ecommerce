034667869b77e5a6c6b08cbcbc3dc6f26f9d19f2
GustavoBryan <devgustavob@gmail.com>
2023-04-14 10:01:59 -0300

    cria produtos

---
+++ controllers/cria-produtos-controller.js
@@ -0,0 +1,21 @@
+import { produtoServices } from "../services/produto-servicos.js";
+
+const form = document.querySelector("[data-form]");
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+
+    const nome = document.querySelector("[data-nome]").value;
+    const url = document.querySelector("[data-url]").value;
+    const preco = document.querySelector("[data-preco]").value;
+
+    produtoServices
+        .criaProdutos(nome, url, preco)
+        .then((resposta) => {
+            window.location.href = "../views/index.html";
+            console.log(resposta);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
